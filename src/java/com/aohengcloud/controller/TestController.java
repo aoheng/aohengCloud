f92e2e0914ac2da6757713e23193885ee364ee6f
aoheng <l>
2018-12-27 11:24:43 +0800

    gds

---
+++ src/java/com/aohengcloud/controller/TestController.java
@@ -0,0 +1,12 @@
+package java.com.aohengcloud.controller;
+
+/**
+ * @Author:kemingheng
+ * @Date:2018/12/27
+ */
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
