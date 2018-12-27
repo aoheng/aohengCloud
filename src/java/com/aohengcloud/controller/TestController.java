ffa44e2724f09153a461c6a4a9efe68bbedcca11
aoheng <l>
2018-12-27 11:36:50 +0800

    idea 整合github测试

--- src/java/com/aohengcloud/controller/TestController.java
+++ src/java/com/aohengcloud/controller/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
         for (int i = 0; i < 10; i++) {
             System.out.println("hello world"+i);
             String s = showLove();
-            System.out.println("============"+s);
+            System.out.println("====fgdf========"+s);
         }
     }
 
