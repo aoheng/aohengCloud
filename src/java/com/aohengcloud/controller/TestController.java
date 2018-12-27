34fadddd1e68b53262f283347869c06140d0726a
aoheng <l>
2018-12-27 11:31:48 +0800

    idea 整合github

--- src/java/com/aohengcloud/controller/TestController.java
+++ src/java/com/aohengcloud/controller/TestController.java
@@ -7,6 +7,10 @@ package java.com.aohengcloud.controller;
 public class TestController {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello world"+i);
+        }
     }
+
+
 }
